3ceed227e4e4998e6b7c9f2429203f9909acef29
coltiq <dev@coltiq.com>
2024-07-12 21:23:33 -0500

    Split: Fileserver check

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"path/filepath"
+)
 
 func (cfg *apiConfig) routes() *http.ServeMux {
 	mux := http.NewServeMux()
@@ -14,4 +17,38 @@ func (cfg *apiConfig) routes() *http.ServeMux {
 	mux.HandleFunc("POST /snippets/create", cfg.snippetsCreatePost) // Save a new snippet w/ POST restriction
 
 	return mux
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if err != nil {
+		closeErr := f.Close()
+		if closeErr != nil {
+			return nil, closeErr
+		}
+		return nil, err
+	}
+
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
