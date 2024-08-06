12a36c1b1ac9d1d5cccc9785e48453cc21fd9e53
coltiq <dev@coltiq.com>
2024-08-06 11:20:47 -0500

    Add: TLS

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"log/slog"
@@ -58,14 +59,22 @@ func main() {
 		sessionManager: sessionManager,
 	} // Config struct containing dependencies
 
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
 	srv := &http.Server{
-		Addr:              *addr,
-		Handler:           app.routes(),
-		ReadHeaderTimeout: 10 * time.Second,
+		Addr:         *addr,
+		Handler:      app.routes(),
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	} // Server Config
 
-	logger.Info("starting server", slog.String("addr", *addr))
-	err = srv.ListenAndServe()
+	logger.Info("starting server", "addr", srv.Addr)
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	logger.Error(err.Error())
 	os.Exit(1)
 }
