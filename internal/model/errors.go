519832a521af3ffe32f29ab773574667ab7f3234
coltiq <dev@coltiq.com>
2024-08-07 11:05:18 -0500

    Feat: User Signup

--- internal/model/errors.go
+++ internal/model/errors.go
@@ -2,4 +2,8 @@ package model
 
 import "errors"
 
-var ErrNoRecord = errors.New("model: no matching record found")
+var (
+	ErrNoRecord           = errors.New("model: no matching record found")
+	ErrInvalidCredentials = errors.New("model: invalid crendentials")
+	ErrDuplicateEmail     = errors.New("model: duplicate email")
+)
