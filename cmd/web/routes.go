1efc82c9aa6afc021e08158aa23d41295292235e
coltiq <dev@coltiq.com>
2024-08-08 16:37:50 -0500

    Add: Authentication

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,17 +14,17 @@ func (app *application) routes() http.Handler {
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer)) // Server static files
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))                        // Restrict this route to exact matches on / only
+	mux.Handle("GET /snippets/view/{id}", dynamic.ThenFunc(app.snippetsView)) // Display a specific snippet w/ {id} wildcard
+	mux.Handle("GET /users/signup", dynamic.ThenFunc(app.userSignup))         // Display a form for signing up a new user
+	mux.Handle("POST /users/signup", dynamic.ThenFunc(app.userSignupPost))    // Create a new user
+	mux.Handle("GET /users/login", dynamic.ThenFunc(app.userLogin))           // Display a form for logging in a user
+	mux.Handle("POST /users/login", dynamic.ThenFunc(app.userLoginPost))      // Authenticate and login the user
 
-	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))                            // Restrict this route to exact matches on / only
-	mux.Handle("GET /snippets/view/{id}", dynamic.ThenFunc(app.snippetsView))     // Display a specific snippet w/ {id} wildcard
-	mux.Handle("GET /snippets/create", dynamic.ThenFunc(app.snippetsCreate))      // Disply a form to create a new snippet w/ GET restriction
-	mux.Handle("POST /snippets/create", dynamic.ThenFunc(app.snippetsCreatePost)) // Save a new snippet w/ POST restriction
-
-	mux.Handle("GET /users/signup", dynamic.ThenFunc(app.userSignup))      // Display a form for signing up a new user
-	mux.Handle("POST /users/signup", dynamic.ThenFunc(app.userSignupPost)) // Create a new user
-	mux.Handle("GET /users/login", dynamic.ThenFunc(app.userLogin))        // Display a form for logging in a user
-	mux.Handle("POST /users/login", dynamic.ThenFunc(app.userLoginPost))   // Authenticate and login the user
-	mux.Handle("POST /users/logout", dynamic.ThenFunc(app.userLogoutPost)) // Logout the user
+	protected := dynamic.Append(app.requireAuthentication)
+	mux.Handle("GET /snippets/create", protected.ThenFunc(app.snippetsCreate))      // Disply a form to create a new snippet w/ GET restriction
+	mux.Handle("POST /snippets/create", protected.ThenFunc(app.snippetsCreatePost)) // Save a new snippet w/ POST restriction
+	mux.Handle("POST /users/logout", protected.ThenFunc(app.userLogoutPost))        // Logout the user
 
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 
